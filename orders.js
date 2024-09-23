9bfce0f8cc2d9873ac61b407130af84f2277fae0
Burak-Cevik <143699630+Burak-Cevik@users.noreply.github.com>
2024-09-23 09:14:02 +0200

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,44 @@
+const orders = [
+    {
+        productName: 'Paella',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Tortilla',
+        productNumber: '36378',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Patatas Bravas',
+        productNumber: '36378',
+        paymentStatus: 'paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Jamon',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Croquettes',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Churos',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Turron',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+];
